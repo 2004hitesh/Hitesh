80955e7f6efe077584e6e8da5228f1910d39d51a
hitesh kumar <git.hitesh@gmail.com>
2024-03-18 19:10:09 +0530

    merge ke pahela ka change

--- SHA.h
+++ SHA.h
@@ -1,2 +1,3 @@
 # define motherboard RAMP 1.4 
 # define steps {80,80,300,270}
+# define direction false 
--- hitesh.js
+++ hitesh.js
@@ -1,3 +1,7 @@
 console.log(" this is demo folder");
 let a=100;
 let abc=233;
+
+function add(){
+    console.log("git hub")
+}
